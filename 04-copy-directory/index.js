f6f21a0970507f79f44657ecbe15a8b99ecb0f74
Wokc <mozgomozgo93@mail.ru>
2023-05-09 17:59:27 +0300

    feat: task 4 done

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,24 @@
+const fs = require('fs');
 
+const path = require('path');
+
+const fsPromises = fs.promises;
+
+let pathCopy = path.join(__dirname, 'files-copy');
+let pathOriginal = path.join(__dirname, 'files');
+
+fs.mkdir(pathCopy, { recursive: true }, (err, data) => {
+    if (err) throw err;
+    else {
+        fs.readdir(pathOriginal, (err, files) => {
+            if (err) throw err;
+            else {
+                files.forEach((file) => {
+                    fs.copyFile(path.join(pathOriginal, file), path.join(pathCopy, file), (err) => {
+                        if (err) throw err;
+                    });
+                });
+            }
+        });
+    }
+});
